1ebed5dbe64dfe6f85681c40eac9649c840a0b5d
greatkene <ihevuemeg@gmail.com>
2024-06-27 22:45:33 +0100

    chore: shiga font

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,12 +1,5 @@
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
-import {
-  OpenSans_300Light,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_600SemiBold,
-  OpenSans_700Bold,
-} from "@expo-google-fonts/open-sans";
 
 export const useCachedResources = () => {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -15,11 +8,9 @@ export const useCachedResources = () => {
     async function loadResourcesAndDataAsync() {
       try {
         await Font.loadAsync({
-          "sans-light": OpenSans_300Light,
-          "sans-regular": OpenSans_400Regular,
-          "sans-medium": OpenSans_500Medium,
-          "sans-semi-bold": OpenSans_600SemiBold,
-          "sans-bold": OpenSans_700Bold,
+          "sans-regular": require("@assets/font/ShigaSans-Regular.otf"),
+          "sans-medium": require("@assets/font/ShigaSans-Medium.otf"),
+          "sans-bold": require("@assets/font/ShigaSans-Bold.otf"),
         });
       } catch (error) {
         console.warn(error);
